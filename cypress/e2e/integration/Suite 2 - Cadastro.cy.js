c26b6a21d0fde59e52410b3a5540cc421af3b010
danieli-santos <dannysantos1106@gmail.com>
2023-04-02 21:45:42 -0300

    incluir CT2 CT3

--- cypress/e2e/integration/Suite 2 - Cadastro.cy.js
+++ cypress/e2e/integration/Suite 2 - Cadastro.cy.js
@@ -1,18 +1,37 @@
+import {cadastro} from '../utils'
 describe('Cadastro', () => {
-  it('Cenario 1', {tags: 'Cadastro'}, () => {
-    //- Acesse https://www.amazon.com.br/
-    cy.visit('https://www.amazon.com.br/')  
-    //- Acesse a página para criar uma nova conta
-    cy.contains('Olá, faça seu login').click()
-    cy.contains('Fazer login').should('have.text', '\n            Fazer login\n          ')
-    cy.get('[id="createAccountSubmit"]').click()
+
+  beforeEach(() => {
+        //- Acesse https://www.amazon.com.br/
+        cy.visit('https://www.amazon.com.br/')
     
-    //- Preencha os campos
-    cy.contains('Criar conta').should('have.text', '\n            Criar conta\n          ')
-    cy.get('[type = text]').type('Danieli Santos')
-    cy.get('#ap_email').type('12999999999')
-    cy.get('[id="ap_password"]').type('993594@D')
-    cy.get('[name =passwordCheck]').type('993594@D')
+        //- Acesse a página para criar uma nova conta
+        cy.contains('Olá, faça seu login').click()
+        cy.contains('Fazer login').should('have.text', '\n            Fazer login\n          ')
+        cy.get('[id="createAccountSubmit"]').click()
+        cy.contains('Criar conta').should('have.text', '\n            Criar conta\n          ')
+  });
+
+  it('Cenario 1 - Candastro realizado com sucesso', {tags: 'Cadastro'}, () => { 
+    //- Preencha os campos 
+    cadastro('danieli', 'danieli@hotmail.com', '993594@D', '993594@D')
     cy.get('span').contains('Continuar').should('have.text', 'Continuar')
   })
+
+  it('Cenario 2 - Email invalido', () => {
+    cadastro('danieli', 'danielihotmail.com', '993594@D', '993594@D')
+    cy.get('#continue').click()
+    cy.xpath("//div[@class='a-alert-content'][contains(.,'Endereço de e-mail ou número de telefone celular errado ou inválido. Corrija e tente novamente.')]").should('have.text', '\n  Endereço de e-mail ou número de telefone celular errado ou inválido. Corrija e tente novamente.\n')
+
+  });
+
+  it('Cenario 3 - Telefone invalido', () => {
+    cadastro('danieli', '999999', '993594@D', '993594@D')
+    cy.get('#continue').click()
+    //cy.get('h1').contains('Verificar o número do telefone celular').should('have.text', 'Verificar o número do telefone celular')
+
+
+
+  });
+
 })
--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -10,3 +10,10 @@ export function addItens(iten) {
     cy.get('span').contains('Adicionado ao carrinho').should('have.text', '\nAdicionado ao carrinho\n')
     
 }
+
+export function cadastro(nome, telEmail, senha, verificarSenha){
+    cy.get('[type = text]').type(nome)
+    cy.get('#ap_email').type(telEmail)
+    cy.get('[id="ap_password"]').type(senha)
+    cy.get('[name =passwordCheck]').type(verificarSenha)
+}
